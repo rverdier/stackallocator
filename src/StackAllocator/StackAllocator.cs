51d62672145a667f3d0ed3e367d5fa35da3bbace
r.verdier <romain.verdier@gmail.com>
2015-01-03 22:35:12 +0100

    Create initial project structure

---
+++ src/StackAllocator/StackAllocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace StackAllocator
+{
+    public static class StackAllocator
+    {
+        static void AllocatesString(byte[] bytes, int length, Decoder decoder, Action<string> action)
+        {
+        }
+
+        static void AllocatesArray<T>(int length, Action<T[]> action) where T : struct
+        {
+        }
+    }
+}
